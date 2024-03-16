f5aece2b1870c9ce483adc15cc30d103d81deb31
Gerald Maale <geraldmaale@hotmail.com>
2024-03-16 19:34:26 +0800

    Add UserDataservice class for handling user-related operations

---
+++ src/Web/Dataservices/UserDataservice.cs
@@ -0,0 +1,39 @@
+﻿using GreatIdeas.EducareHigh.Web.DTOs;
+
+namespace GreatIdeas.EducareHigh.Web;
+
+public class UserDataservice
+{
+    private readonly HttpClient _httpClient;
+
+    public UserDataservice(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<UserDto?> GetUser(string id)
+    {
+        var response = await _httpClient.GetFromJsonAsync<UserDto>($"api/user/{id}");
+        return response;
+    }
+
+    public async Task<UserDto?> UpdateUser(UserUpdateDto user)
+    {
+        var response = await _httpClient.PutAsJsonAsync("api/user", user);
+        var userResponse = await response.Content.ReadFromJsonAsync<UserDto>();
+        return userResponse;
+    }
+
+    public async Task<UserDto?> CreateUser(UserCreateDto user)
+    {
+        var response = await _httpClient.PostAsJsonAsync("api/user", user);
+        var userResponse = await response.Content.ReadFromJsonAsync<UserDto>();
+        return userResponse;
+    }
+
+    public async Task DeleteUser(string id)
+    {
+        var response = await _httpClient.DeleteAsync($"api/user/{id}");
+        response.EnsureSuccessStatusCode();
+    }
+}
