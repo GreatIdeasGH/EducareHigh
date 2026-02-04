e280532b087cf97bcfcec43825b436a6c403637a
Gerald Maale <geraldmaale@hotmail.com>
2026-02-04 13:54:25 +0000

    feat: update namespace declarations in dataservice files for consistency

--- src/Web/Dataservices/AccountDataservice.cs
+++ src/Web/Dataservices/AccountDataservice.cs
@@ -1,23 +1,27 @@
 ﻿using GreatIdeas.EducareHigh.Web.DTOs;
 using GreatIdeas.EducareHigh.Web.Responses;
 
-namespace GreatIdeas.EducareHigh.Web;
+namespace GreatIdeas.EducareHigh.Web.Dataservices;
 
 public interface IAccountDataservice
 {
     // Login with username and password
     Task<LoginResponse?> Login(LoginRequest request);
+
     // Register with username and password
     Task<RegisterResponse?> Register(RegisterRequest request);
+
     // Logout
     Task Logout();
+
     // Get the current user
     Task<UserDto?> GetCurrentUser();
+
     // Delete user
     Task DeleteUser(string id);
+
     // Forgot password
     Task ForgotPassword(string email);
-
 }
 
 public class AccountDataservice(HttpClient httpClient) : IAccountDataservice
